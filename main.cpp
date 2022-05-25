3ce23b9d56a7924e2d48891a8f6e0c54c4d9f7f9
Phạm Khôi Nguyên <100278861+21020083@users.noreply.github.com>
2022-05-25 22:42:53 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,15 +3,15 @@
 #include<vector>
 #include <stdio.h>
 #include<string>
-#include"ScoreBoard.h"
+#include"playground.h"
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 640
 #define speed 0,2
 using namespace std;
 enum dir {NONE,UP,LEFT,DOWN,RIGHT}key;
-SDL_Color BLACK{50,50,50};
 SDL_Color WHITE{255,255,255};
 SDL_Color RED{255,0,0};
+Uint32 delay=1300;
 Shape shapes[7]={
     {{177, 85, 217}, // L BLOCK
     {1,5,4,9}
@@ -207,48 +207,6 @@ bool checkCollisionX(Shape s){
     }
     return false;
 }
-
-struct playground{
-    SDL_Color cellstate[20][10];
-    void init(){
-        for(int i=0;i<20;i++)
-            for(int j=0;j<10;j++)
-                cellstate[i][j]=BLACK;
-    }
-    void draw(SDL_Renderer *renderer){
-        for(int i=0;i<20;i++){
-            for(int j=0;j<10;j++){
-                SDL_Rect rect{j*block_size,i*block_size,block_size,block_size};
-                SDL_SetRenderDrawColor(renderer,cellstate[i][j].r,cellstate[i][j].g,cellstate[i][j].b,255);
-                SDL_RenderFillRect(renderer,&rect);
-                SDL_SetRenderDrawColor(renderer,20,20,20,255);
-                SDL_RenderDrawRect(renderer,&rect);
-
-            }
-        }
-
-    };
-    void lock(Shape s,SDL_Renderer* renderer){
-           for(int i=0;i<4;i++){
-                int dx=s.pos[i]%4;
-                int dy=s.pos[i]/4;
-                cellstate[(s.y+dy*block_size)/block_size][(s.x+dx*block_size)/block_size]=s.color;
-                }
-        }
-    bool canMove(Shape s){
-        for(int i=0;i<4;i++){
-                int dx=s.pos[i]%4;
-                int dy=s.pos[i]/4;
-                SDL_Color c=cellstate[(s.y+dy*block_size)/block_size][(s.x+dx*block_size)/block_size];
-                if(c.r !=BLACK.r || c.g !=BLACK.g || c.b !=BLACK.g)
-                    return false;
-                }
-            return true;
-        };
-        void checkRows(SoundEffects se);
-        void checkGameEnd(Shape s);
-    }
-;
 bool canrotate(Shape s,playground p){
             Shape s1= s;
             s1.rotate();
@@ -257,54 +215,42 @@ bool canrotate(Shape s,playground p){
             return false;
 
 }
-void playground::checkRows(SoundEffects se){
-    int count = 0;
-    for(int i=0;i<20;i++){
-        for(int j=0;j<10;j++){
-                if(cellstate[i][j].r !=BLACK.r || cellstate[i][j].g !=BLACK.g || cellstate[i][j].b !=BLACK.b)
-                    count++;
-        }
-        if(count==10){
-            for(int j=0;j<10;j++){
-               for(int k=i-1;k>=0;k--){
-                SDL_Color tmp=cellstate[k+1][j];
-                cellstate[k+1][j]=cellstate[k][j];
-                cellstate[k][j]=tmp;
-               }
-            }
-            for(int j=0;j<10;j++)
-                cellstate[0][j]=BLACK;
-            score+=100;
-            line+=1;
-            se.playSoundEffect();
-        }
-        count=0;
-    }
 
-}
 
-void playground::checkGameEnd(Shape s)
-{
-    if(!canMove(s) &&  s.y == 0){
-        running = false;
-        m_running=true;
-    }
 
-}
+
+Uint32 timeStep;
+SDL_Window* window=NULL;
+SDL_Renderer* renderer=NULL;
+playground p;
+Shape s;
+ScoreBoard Board;
+
 void fall(Shape&s ,playground& p)
 {
-    Uint32 timeout = SDL_GetTicks() + 100;
-    if (SDL_TICKS_PASSED(SDL_GetTicks(), timeout) && p.canMove(s)){
-        s.y+=block_size;
+    if ( SDL_GetTicks() >= timeStep + delay ){
+        timeStep = SDL_GetTicks();
+        s.y += block_size;
+        if (!p.canMove(s) || checkCollisionY(s))
+            {
+            s.y-=block_size;
+            p.lock(s,renderer);
+            s=Board.next_piece;
+            s.x=0;
+            s.y=0;
+            Board.update();
+            }
     }
 }
+void checkLevel()
+{
+    if(score>=1000)
+    {
+        delay-=300;
+    }
 
- SDL_Window* window=NULL;
- SDL_Renderer* renderer=NULL;
+}
 
- playground p;
- Shape s;
- ScoreBoard Board;
 
 void update(){
     switch(key){
@@ -340,112 +286,30 @@ void update(){
     default:
         break;
     }
+    fall(s,p);
     key=NONE;
-    p.checkRows(se);
-    p.checkGameEnd(s);
-
+    if(p.checkRows())
+    {
+        se.playSoundEffect();
+        score+=100;
+        line+=1;
+    }
+    if(p.checkGameEnd(s))
+    {
+        running = false;
+        m_running = true;
+    }
+    checkLevel();
 };
-void cleargame(playground&p)
+void cleargame(playground &p )
 {
     p.init();
     score=0;
     line=0;
 }
-struct Game
-{
-    playground* p;
-    Shape* s;
-    SDL_Renderer* renderer;
-    SDL_Window* window;
-    SoundEffects *se;
-
-    void GameInput(SDL_Event e){
-         while (SDL_PollEvent(&e))
-    {
-        if (e.type == SDL_QUIT)
-        {
-            running = false;
-        }
-        else if (e.type == SDL_KEYDOWN)
-        {
-            switch (e.key.keysym.sym)
-            {
-                case SDLK_UP:
-                    key=UP;
-                    break;
-
-                case SDLK_DOWN:
-                    key=DOWN;
-                    break;
-
-                case SDLK_LEFT:
-                    key=LEFT;
-                    break;
-
-                case SDLK_RIGHT:
-                    key=RIGHT;
-                    break;
-            }
-        }
-
-        }
-    }
-    void GameUpdate()
-    {
-    switch(key){
-    case UP:{
-        if(canrotate(*s,*p))
-            s->rotate();
-        break;
-    }
-    case LEFT:{
-            s->x-=block_size;
-        if(checkCollisionX(*s) || !p->canMove(*s))
-            s->x+=block_size;
-        break;}
-    case DOWN:{
-        s->y+=block_size;
-        if(checkCollisionY(*s)||!p->canMove(*s)){
-            s->y-=block_size;
-            p->lock(*s,renderer);
-            s->spawn();
-        }
-        break;
-    }
-    case RIGHT:{
-            s->x+=block_size;
-            if(!p->canMove(*s)||checkCollisionX(*s))
-              s->x-=block_size;
-        break;
-            }
-    default:
-        break;
-    }
-    key=NONE;
-    p->checkRows(*se);
-    p->checkGameEnd(*s);
-    };
-   void GameRender()
-    {
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
-        p->draw(renderer);
-        s->draw(renderer);
-        SDL_RenderPresent(renderer);
-    }
-    void gameOver()
-    {
-        s=NULL;
-        p=NULL;
-        se=NULL;
-    }
-};
-
-
-
 int main(int argc, char** argv){
     p.init();
-    s=shapes[4];
+    s=shapes[rand()%7];
 if(SDL_Init(SDL_INIT_VIDEO) < 0){
         printf("Error: SDL failed to initialize\nSDL Error: '%s'\n", SDL_GetError());
         return 1;
@@ -475,7 +339,7 @@ if(SDL_Init(SDL_INIT_VIDEO) < 0){
         se.addSoundEffect("line.wav");
         se.addMusic("sound_bgm.mp3");
         Board.next_piece=shapes[rand()%7];
-
+        timeStep = SDL_GetTicks();
     while(game){
         SDL_Event m_event;
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
